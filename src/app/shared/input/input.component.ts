04594857f8c49e6e479169cb5f9230c6950a2182
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-25 16:53:44 +0200

    Added input type

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,5 +8,11 @@ import {FormControl} from "@angular/forms";
 })
 export class InputComponent {
   @Input() label: string = '';
+  @Input() inputType: string = '';
   @Input() control: FormControl = new FormControl;
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
