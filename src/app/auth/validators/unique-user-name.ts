141f5ff42f7467ef936cba7abd61db7cc95be047
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-25 16:00:54 +0200

    Added auth service

--- src/app/auth/validators/unique-user-name.ts
+++ src/app/auth/validators/unique-user-name.ts
@@ -1,17 +1,28 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {AbstractControl, AsyncValidator} from "@angular/forms";
+import {catchError, map, of} from "rxjs";
+import {AuthService} from "../auth.service";
 
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
 
   validate = (control: AbstractControl) => {
-    const { value } = control;
-    console.log(value)
+    const {value} = control;
 
-    return this.http.post<any>('https://api.angular-email.com/auth/username', {
-      username: value
-    });
+    return this.authService.usernameAvailable(value).pipe(
+      map(() => {
+        return null;
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({nonUniqueUsername: true})
+        } else {
+          return of({noConnection: true})
+        }
+      })
+    );
   };
 }
