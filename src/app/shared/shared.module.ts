fffd52ec7a3f0b35ea39fa224b36af65dde528db
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-26 17:38:43 +0200

    Added modal component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { ModalComponent } from './modal/modal.component';
 
 
 
 @NgModule({
   declarations: [
-    InputComponent
+    InputComponent,
+    ModalComponent
   ],
   exports: [
-    InputComponent
+    InputComponent,
+    ModalComponent
   ],
   imports: [
     CommonModule,
