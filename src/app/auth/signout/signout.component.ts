73ce3ab1113f3169526082d33f595508fcb6fba8
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-26 13:27:21 +0200

    Added router after sign out

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signout',
@@ -7,11 +8,13 @@ import {AuthService} from "../auth.service";
   styleUrls: ['./signout.component.scss']
 })
 export class SignoutComponent implements OnInit{
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
 
   }
 
   ngOnInit(): void {
-    this.authService.signOut().subscribe(()=>{})
+    this.authService.signOut().subscribe(()=>{
+      this.router.navigateByUrl('/');
+    })
   }
 }
