85ac583c61439aa102fe6dd596c9cb951f97ac88
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-26 15:26:30 +0200

    added email module

---
+++ src/app/inbox/email.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+interface EmailSummary{
+  id:string,
+  subject:string,
+  from:string
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  baseUrl = 'https://api.angular-email.com/';
+  constructor(private http: HttpClient) { }
+
+  getEmails(){
+    return this.http.get<EmailSummary[]>(`${this.baseUrl}emails`)
+  }
+}
